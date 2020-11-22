e59d0a3113bb872c62aec4040e82f2ce077b7095
yokokazkaz <71754586+yokokazkaz@users.noreply.github.com>
2020-11-23 00:23:24 +0900

    factory userモデルの追加

---
+++ spec/factories/users.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :user do
+    transient do
+      person { Gimei.name }
+    end
+
+    nickname              { Faker::Name.name }
+    email                 { Faker::Internet.free_email }
+    password              { '1a' + Faker::Internet.password(min_length: 6) }
+    password_confirmation { password }
+    last_name             { person.last.kanji }
+    first_name            { person.first.kanji }
+  end
+end
