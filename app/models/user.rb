b2ec69d5be73e84f7be52253727eb9ec54a31322
yokokazkaz <71754586+yokokazkaz@users.noreply.github.com>
2020-11-22 22:36:31 +0900

    userモデルのバリデーション設定とエラーメッセージ表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+        
+         with_options presence: true do
+          validates :nickname
+          VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+          validates :email, uniqueness: true, format: { with: VALID_EMAIL_REGEX}, allow_blank: true
+          VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}\z/i.freeze
+          validates :password, confirmation: true, format: { with: VALID_PASSWORD_REGEX}, allow_blank: true
+          validates :last_name, format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/, allow_blank: true }
+          validates :first_name, format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/, allow_blank: true }
+         end
 end
