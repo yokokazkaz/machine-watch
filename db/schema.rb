ea7bde53fddb1db38b9faeebb3c5fb035da952b8
yokokazkaz <71754586+yokokazkaz@users.noreply.github.com>
2020-11-23 12:28:44 +0900

    machinesテーブルの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_22_125545) do
+ActiveRecord::Schema.define(version: 2020_11_23_032226) do
 
   create_table "machines", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC", force: :cascade do |t|
     t.string "name", null: false
     t.text "detail", null: false
+    t.boolean "checked"
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_machines_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC", force: :cascade do |t|
@@ -34,4 +37,5 @@ ActiveRecord::Schema.define(version: 2020_11_22_125545) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "machines", "users"
 end
