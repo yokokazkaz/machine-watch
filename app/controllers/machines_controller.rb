cb5840641333728aaf5b0bff23fab6b7d416554b
yokokazkaz <71754586+yokokazkaz@users.noreply.github.com>
2020-11-23 12:36:24 +0900

    createアクションとmachine_paramsメソッドの設定

--- app/controllers/machines_controller.rb
+++ app/controllers/machines_controller.rb
@@ -7,4 +7,19 @@ class MachinesController < ApplicationController
     @machine = Machine.new
   end
 
+def create
+  @machine = Machine.new(machine_params)
+    if @machine.save
+      redirect_to root_path
+    else
+      render :new
+    end
+end
+
+private
+
+def machine_params
+  params.require(:machine).permit(:name, :detail, :image).merge(user_id: current_user.id)
+end
+
 end
