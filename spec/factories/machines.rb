f43c75a7c5974142772f329c01da86d09a3e631c
yokokazkaz <71754586+yokokazkaz@users.noreply.github.com>
2020-11-23 21:36:51 +0900

    factory bot の設定

--- spec/factories/machines.rb
+++ spec/factories/machines.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :machine do
-    
+    name              { 'インクラインベンチA' }
+    detail            { 'インクラインベンチAです' }
+    association :user
+
+    after(:build) do |machine|
+      machine.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
