c93b97d0903b53f19bfdd7ddedd1a9b882e0deb6
yokokazkaz <71754586+yokokazkaz@users.noreply.github.com>
2020-11-26 22:42:50 +0900

    editアクションのルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "machines#index"
-  resources :machines, only: [:index, :new, :create, :show]
+  resources :machines, only: [:index, :new, :create, :show, :edit]
 end
