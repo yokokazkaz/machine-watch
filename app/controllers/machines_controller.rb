6bfa3261990bafb669e51440e2d51c411710a95d
yokokazkaz <71754586+yokokazkaz@users.noreply.github.com>
2020-11-27 13:18:58 +0900

    destoryアクションの定義

--- app/controllers/machines_controller.rb
+++ app/controllers/machines_controller.rb
@@ -35,6 +35,13 @@ class MachinesController < ApplicationController
     end
   end
 
+  def destroy
+    if current_user.id == @item.user_id
+        @item.destroy
+        redirect_to root_path
+    end
+  end
+
   private
 
   def machine_params
