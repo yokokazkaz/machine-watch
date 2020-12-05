e87ce5529b2d04f24f173577255553e838d5f885
yokokazkaz <71754586+yokokazkaz@users.noreply.github.com>
2020-12-05 15:09:44 +0900

    checked.jsの削除

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,7 +6,7 @@
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
-require("../checked.js")
+
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
